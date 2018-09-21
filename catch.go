45d78aabe35f9a72c7980b4dffa7503c47819187
Shmelev Ivan <i.shmelev@corp.mail.ru>
2018-09-21 12:57:18 +0300

    fix-chain-data fixed

--- catch.go
+++ catch.go
@@ -1,8 +1,12 @@
 package babex
 
+import (
+	"encoding/json"
+)
+
 type CatchData struct {
-	Error    string `json:"error"`
-	Exchange string `json:"exchange"`
-	Key      string `json:"key"`
-	Data     []byte `json:"data"`
+	Error    string          `json:"error"`
+	Exchange string          `json:"exchange"`
+	Key      string          `json:"key"`
+	Data     json.RawMessage `json:"data"`
 }
