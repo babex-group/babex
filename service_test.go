0afbdd433675a2c4f89d6dd235a1b65b9f905426
valentin.dubrovskiy <spbclan@gmail.com>
2018-10-28 17:47:11 +0300

    add tests for receive and middleware

---
+++ service_test.go
@@ -0,0 +1,76 @@
+package babex
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestService_Receive(t *testing.T) {
+	ch := make(chan *Message)
+
+	stub := StubAdapter{OutCh: ch}
+	s := NewService(&stub)
+
+	expectedMsg := &Message{Exchange: "test", Key: "test-key"}
+
+	ch <- expectedMsg
+
+	select {
+	case msg := <-s.GetMessages():
+		assert.Equal(t, expectedMsg, msg)
+	case <-time.After(time.Microsecond * 1):
+		panic("cannot read message")
+	}
+
+	expectedMsg1 := &Message{Exchange: "test1", Key: "test-key1"}
+
+	ch <- expectedMsg1
+
+	select {
+	case msg := <-s.GetMessages():
+		assert.Equal(t, expectedMsg1, msg)
+	case <-time.After(time.Microsecond * 1):
+		panic("cannot read message")
+	}
+}
+
+func TestService_AppyMiddleware(t *testing.T) {
+	var isUse bool
+	var isFinish bool
+
+	ch := make(chan *Message)
+
+	middleware := StubMiddleware{
+		OnUse: func(msg *Message) (MiddlewareDone, error) {
+			isUse = true
+
+			return func(err error) {
+				isFinish = true
+			}, nil
+		},
+	}
+
+	stub := StubAdapter{OutCh: ch}
+	s := NewService(&stub, middleware)
+
+	expectedMsg := &Message{
+		Exchange: "test",
+		Key: "test-key",
+		RawMessage: StubMessage{},
+	}
+
+	ch <- expectedMsg
+
+	select {
+	case msg := <-s.GetMessages():
+		assert.Equal(t, expectedMsg, msg)
+
+		s.Next(msg, nil, nil)
+	case <-time.After(time.Microsecond * 1):
+		panic("cannot read message")
+	}
+
+	assert.True(t, isUse)
+	assert.True(t, isFinish)
+}
--- stub.go
+++ stub.go
@@ -18,3 +18,39 @@ func (sm StubMessage) Ack() error {
 func (sm StubMessage) Nack() error {
 	return nil
 }
+
+type StubAdapter struct {
+	OutCh       chan *Message
+	OutChannels Channels
+	Err         chan error
+
+	onPublish func(exchange string, key string, message InitialMessage) error
+}
+
+func (stub *StubAdapter) GetMessages() (<-chan *Message, error) {
+	return stub.OutCh, nil
+}
+
+func (stub *StubAdapter) GetErrors() chan error {
+	return stub.Err
+}
+
+func (stub *StubAdapter) Publish(exchange string, key string, message InitialMessage) error {
+	return stub.onPublish(exchange, key, message)
+}
+
+func (stub *StubAdapter) Close() error {
+	return nil
+}
+
+func (stub *StubAdapter) Channels() Channels {
+	return stub.OutChannels
+}
+
+type StubMiddleware struct {
+	OnUse func(msg *Message) (MiddlewareDone, error)
+}
+
+func (stubMiddleware StubMiddleware) Use(msg *Message) (MiddlewareDone, error) {
+	return stubMiddleware.OnUse(msg)
+}
